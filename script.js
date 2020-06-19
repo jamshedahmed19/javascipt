32162f7b70efe0e28def6c8038de7590331770e1
Jamshed Ahmed <jamsheda4ahmed786@gmail.com>
2020-06-19 20:25:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 // strings in js
 
+console.log("Hello, World");
+let name = "jamshed ahmed";
+console.log(name);
+
+//string concatenation
+let firstName = "Jamshed";
+let lastName = "Ahmed";
+name = firstName + " " + lastName;
+console.log(name);
+
+//string length
+console.log(name.length);
+
+//getting characters from a string
+console.log(name[8]);
+
+//string methods
+console.log(name.toUpperCase());
+name = name.toLowerCase();
+console.log(name);
+console.log(name.indexOf("m"));
+console.log(name.replace("m", "n"));
+console.log(name.slice(4, 10));
+console.log(name.substr(4, 10));
+console.log(name.lastIndexOf("m"));
