68a5f852a5cec77be6c80efd04e5335a961abf14
Jamshed Ahmed <jamsheda4ahmed786@gmail.com>
2020-06-19 19:38:38 +0500

    variables in Js

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 //Printing using Pop-Ups
-alert('Hello, World');
+//alert('Hello, World');
 //Printing in console
-console.log(1);
+//console.log(1);
+
+// variable in Javascript
+
+//Modern Methods
+//Declaring a variable
+let age;
+
+//Assigning value to variable
+age = 19;
+
+//Initializing a variable
+let name = "Jamshed Ahmed";
+
+//Printing variable
+
+//Printing single variable
+console.log(name);
+
+//Printing Multiple variables
+console.log(name, age);
+console.log(name + " is " + age + " years old");
+console.log(`${name} is ${age} years old`);
